30d6b902385be58ba4be0e7d55911a23cf8e51fa
Jaume Parrot <jparrot@gps.local>
2020-06-03 20:54:20 +0200

    VisibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
 
   run(player) {
@@ -12,6 +13,8 @@ class AutoPause {
     });
 
     observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange", this.handleVisibilityChange)
   }
 
   handleIntersection(entries) {
@@ -23,6 +26,15 @@ class AutoPause {
       this.player.play();
     } else {
       this.player.pause();
+    }
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === "visible"
+    if (isVisible) {
+      this.player.play()
+    } else {
+      this.player.pause()
     }
   }
 }
