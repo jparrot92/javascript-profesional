8f711be4d8183089fb552a9c2348396e3078a872
Jaume Parrot <jparrot@gps.local>
2020-06-06 20:53:04 +0200

    Clases

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,56 +1,58 @@
 // Clase media player
-function MediaPlayer(config) {
-  this.media = config.element
-  this.plugins = config.plugins || []
+class MediaPlayer {
 
-  this._initPlugins();
-}
-
-MediaPlayer.prototype._initPlugins = function() {
-  const player = {
-    play: () => this.play(),
-    pause: () => this.pause(),
-    media: this.media,
-    get muted() {
-      return this.media.muted;
-    },
-    set muted(value) {
-      this.media.muted = value;
-    },
-  };
-
-  this.plugins.forEach(plugin => {
-    plugin.run(player);
-  });
-};
-
-// Metodo para reproducir el video
-MediaPlayer.prototype.play = function () {
-  this.media.play();
-};
-
-// Metodo para pausar el video
-MediaPlayer.prototype.pause = function () {
-  this.media.pause();
-};
-
-// Metodo para reproducir/pausar el video
-MediaPlayer.prototype.togglePlay = function () {
-  if (this.media.paused) {
-    this.play();
-  } else {
-    this.pause();
+  constructor(config) {
+    this.media = config.element;
+    this.plugins = config.plugins || [];
+    this._initPlugins();
   }
-};
 
-// Metodo para silenciar el video
-MediaPlayer.prototype.mute = function() {
-  this.media.muted = true;
-};
+  _initPlugins() {
+    const player = {
+      play: () => this.play(),
+      pause: () => this.pause(),
+      media: this.media,
+      get muted() {
+        return this.media.muted;
+      },
+      set muted(value) {
+        this.media.muted = value;
+      },
+    };
+    this.plugins.forEach(plugin => {
+      plugin.run(player);
+    });
+  }
 
-// Metodo para dessilenciar el video
-MediaPlayer.prototype.unmute = function() {
-  this.media.muted = false;
-};
+  // Metodo para reproducir el video
+  play() {
+    this.media.play();
+  }
+
+  // Metodo para pausar el video
+  pause() {
+    this.media.pause();
+  }
+
+  // Metodo para reproducir/pausar el video
+  togglePlay() {
+    if (this.media.paused) {
+      this.play();
+    }
+    else {
+      this.pause();
+    }
+  }
+
+  // Metodo para silenciar el video
+  mute() {
+    this.media.muted = true;
+  }
+
+  // Metodo para dessilenciar el video
+  unmute() {
+    this.media.muted = false;
+  }
+}
 
 export default MediaPlayer
